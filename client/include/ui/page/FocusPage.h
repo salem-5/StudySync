bd156601b03cae50d9c45b288b45aed08b9ed07e
Youssef Micheal <YoussefMicheal@aucegypt.edu>
2026-04-06 13:47:08 +0200

    Added a 25 minute timer in the focus session which allows the user to start, pause and reset

--- client/include/ui/page/FocusPage.h
+++ client/include/ui/page/FocusPage.h
@@ -13,6 +13,7 @@
 #include <QCheckBox>
 #include <QGroupBox>
 #include <QFrame>
+#include <QTimer>
 #include "DataStructures.h"
 
 
@@ -20,4 +21,20 @@ class FocusPage : public QWidget {
     Q_OBJECT
 public:
     FocusPage(QWidget* parent = nullptr);
+
+private slots:
+    void updateTimer();
+    void startTimer();
+    void pauseTimer();
+    void resetTimer();
+
+private:
+    void refreshTimerLabel();
+
+    QTimer* timer;
+    QLabel* timerLabel;
+    QPushButton* startButton;
+    QPushButton* pauseButton;
+    QPushButton* resetButton;
+    int remainingSeconds;
 };
--- client/src/ui/page/FocusPage.cpp
+++ client/src/ui/page/FocusPage.cpp
@@ -6,7 +6,9 @@
 #include <QFile>
 #include "LanguageManager.h"
 
-FocusPage::FocusPage(QWidget* parent) : QWidget(parent) {
+static constexpr int DefaultFocusDurationSeconds = 25 * 60;
+
+FocusPage::FocusPage(QWidget* parent) : QWidget(parent), timer(new QTimer(this)), remainingSeconds(DefaultFocusDurationSeconds) {
     QVBoxLayout* layout = new QVBoxLayout(this);
 
     QGroupBox* group = new QGroupBox(LanguageManager::tr("focus.timer_title"));
@@ -14,22 +16,82 @@ FocusPage::FocusPage(QWidget* parent) : QWidget(parent) {
     groupLayout->setAlignment(Qt::AlignCenter);
 
     QComboBox* taskSelect = new QComboBox();
-    taskSelect->addItem("Review Cell Division Notes");
+    taskSelect->addItem(LanguageManager::tr("focus.task_placeholder"));
 
-    QLabel* timerLabel = new QLabel("25:00");
+    timerLabel = new QLabel();
     QFont timerFont = timerLabel->font();
     timerFont.setPointSize(48);
     timerLabel->setFont(timerFont);
     timerLabel->setAlignment(Qt::AlignCenter);
+    refreshTimerLabel();
 
     QHBoxLayout* btnLayout = new QHBoxLayout();
-    btnLayout->addWidget(new QPushButton(LanguageManager::tr("focus.start")));
-    btnLayout->addWidget(new QPushButton(LanguageManager::tr("focus.pause")));
-    btnLayout->addWidget(new QPushButton(LanguageManager::tr("focus.reset")));
+    startButton = new QPushButton(LanguageManager::tr("focus.start"));
+    pauseButton = new QPushButton(LanguageManager::tr("focus.pause"));
+    resetButton = new QPushButton(LanguageManager::tr("focus.reset"));
+
+    pauseButton->setEnabled(false);
+
+    btnLayout->addWidget(startButton);
+    btnLayout->addWidget(pauseButton);
+    btnLayout->addWidget(resetButton);
 
     groupLayout->addWidget(taskSelect);
     groupLayout->addWidget(timerLabel);
     groupLayout->addLayout(btnLayout);
 
     layout->addWidget(group);
+
+    connect(timer, &QTimer::timeout, this, &FocusPage::updateTimer);
+    connect(startButton, &QPushButton::clicked, this, &FocusPage::startTimer);
+    connect(pauseButton, &QPushButton::clicked, this, &FocusPage::pauseTimer);
+    connect(resetButton, &QPushButton::clicked, this, &FocusPage::resetTimer);
+}
+
+void FocusPage::refreshTimerLabel() {
+    int minutes = remainingSeconds / 60;
+    int seconds = remainingSeconds % 60;
+    timerLabel->setText(QString("%1:%2").arg(minutes, 2, 10, QChar('0')).arg(seconds, 2, 10, QChar('0')));
+}
+
+void FocusPage::updateTimer() {
+    if (remainingSeconds <= 0) {
+        timer->stop();
+        startButton->setEnabled(true);
+        pauseButton->setEnabled(false);
+        return;
+    }
+
+    remainingSeconds -= 1;
+    refreshTimerLabel();
+
+    if (remainingSeconds <= 0) {
+        timer->stop();
+        startButton->setEnabled(true);
+        pauseButton->setEnabled(false);
+    }
+}
+
+void FocusPage::startTimer() {
+    if (!timer->isActive()) {
+        timer->start(1000);
+        startButton->setEnabled(false);
+        pauseButton->setEnabled(true);
+    }
+}
+
+void FocusPage::pauseTimer() {
+    if (timer->isActive()) {
+        timer->stop();
+        startButton->setEnabled(true);
+        pauseButton->setEnabled(false);
+    }
+}
+
+void FocusPage::resetTimer() {
+    timer->stop();
+    remainingSeconds = DefaultFocusDurationSeconds;
+    refreshTimerLabel();
+    startButton->setEnabled(true);
+    pauseButton->setEnabled(false);
 }
